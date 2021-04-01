5992a4138f7cd0dba535881d8c326cfba52b34b1
Gabrielle Araujo <tgobzrj@live.com>
2021-04-01 14:55:11 -0300

    desafio

---
+++ desafios/JS/funcao.js
@@ -0,0 +1,5 @@
+//Não é necessário criar funções para cara botão, apenas uma função com um parametro, criar um alerta com um templatestring chamado o parametro.
+//Quando o botão é clicado o parametro atribuido no HTML é colocado na função.
+function d001(numero_do_botao) {
+    alert(`você clicou no ${numero_do_botao}º botão`)
+}
